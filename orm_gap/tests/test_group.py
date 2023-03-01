1f282858d9296bd74d9de33f446d8b72e8251da7
Victor Hernandez <victor.993_@hotmail.com>
2023-03-01 11:36:24 -0500

    m

---
+++ orm_gap/tests/test_group.py
@@ -0,0 +1,10 @@
+from mongoengine import Document, StringField, ReferenceField
+from .test_crop import Crop
+
+class Group(Document):
+    meta = {
+        'collection': 'group'
+    }
+    group_name = StringField(max_length=255, required=True)
+    crop = ReferenceField(Crop, required=True)
+    ext_id = StringField(max_length=100)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orm_gap",
+    version="0.0.1",
+    author="victor993",
+    author_email="v.hernandez@cgiar.com",
+    description="ORM para la base de datos de gap analysis",
+    url="https://github.com/victor-993/pru",
+    packages=find_packages(),
+    keywords='mongodb orm gap-analysis',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "mongoengine"
+    ]
+)
