8153cf9a32f21bef3eda2d10f325ed9b4f21a4bf
Victor Hernandez <victor.993_@hotmail.com>
2023-03-03 13:59:20 -0500

    e

---
+++ ormgap/tests/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+orm_dir_path = os.path.abspath(os.path.join(dir_path, '..'))
+sys.path.append(orm_dir_path)
+
+# A continuación, importar todos los módulos que necesites para las pruebas
+from orm.models.accession import Accession
+from orm.models.crop import Crop
+from orm.models.group import Group
