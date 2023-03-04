9e7a104250d12b9a05fbe98311e3794eba727309
victor-993 <victor-993@users.noreply.github.com>
2023-03-04 03:31:22 +0000

    Update version to v0.0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ormgap",
-    version='v0.0.16',
+    version='v0.0.17',
     author="victor993",
     author_email="v.hernandez@cgiar.com",
     description="ORM para la base de datos de gap analysis",
